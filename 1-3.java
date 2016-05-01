304578a1e8f5469dff3a7c82b8b1dedeec6b9057
hogarthdl <hogarthdl@gmail.com>
2016-05-01 09:53:12 +0800

    Create 1-3.java

---
+++ 1-3.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/**
+ * Created by hogarth on 2016/5/1.
+ * 1.3 Given two strings, write a method to decide if one is a permutation of the other.
+ */
+
+public class Same {
+    public boolean checkSam(String stringA, String stringB) {
+        char[] cha = stringA.toCharArray();
+        Arrays.sort(cha);
+        char[] chb = stringB.toCharArray();
+        Arrays.sort(chb);
+             
+        if(Arrays.toString(cha).equals(Arrays.toString(chb)))
+            return true;
+        else
+            return false;
+    }
+}
